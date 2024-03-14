edcb95640b8f498e111572a8284048d423ec8115
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-03-14 15:14:46 +0530

    Create tailwind Properties

--- src/pages/Workshops/Workshops.jsx
+++ src/pages/Workshops/Workshops.jsx
@@ -7,12 +7,14 @@ import { WebTeamCard } from "../../constants";
 
 import WebCard from "../../components/Shared/WebCard";
 
+// import Header from "../../components/Header/Header";
 
 
 const Workshops = () => {
 
   return (
     <main>
+<Header/>
       <WorkshopsBanner />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
