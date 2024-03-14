8de5ac6dd3be9c6f8ad73a0a038eeaafb2ddca99
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-03-14 16:40:29 +0530

    Create tailwind Properties

--- src/pages/Workshops/Workshops.jsx
+++ src/pages/Workshops/Workshops.jsx
@@ -1,34 +1,30 @@
 // import ETWSCard from '../../components/Shared/ETWSCard'
 // import Style from './Workshops.module.css'
-import Header from '../../components/Header/Header'
-import ETWSCard from '../../components/Shared/ETWSCard'
-import WorkshopsBanner from './WorkshopsBanner'
+import Header from "../../components/Header/Header";
+import ETWSCard from "../../components/Shared/ETWSCard";
+import WorkshopsBanner from "./WorkshopsBanner";
 import { WebTeamCard } from "../../constants";
 
 import WebCard from "../../components/Shared/WebCard";
 
 // import Header from "../../components/Header/Header";
 
-
 const Workshops = () => {
-
   return (
     <main>
-<Header/>
+      <Header />
       <WorkshopsBanner />
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {WebTeamCard.map((t) => (
+          {WebTeamCard.map((t) => (
             <div key={t.id} className="flex justify-center">
               <WebCard img={t.img} name={t.name} role={t.role} />
             </div>
           ))}
-
         </div>
       </div>
-        
     </main>
-  )
-}
+  );
+};
 
-export default Workshops
+export default Workshops;
