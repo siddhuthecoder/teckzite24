c611d8653c0efedac599e4b67885a4087e080ebf
siddhu_from_sklm <codewithsiddhu@gmail.com>
2024-03-14 14:35:55 +0530

    Create tailwind Properties

--- src/pages/Workshops/Workshops.jsx
+++ src/pages/Workshops/Workshops.jsx
@@ -1,4 +1,4 @@
-import ETWSCard from '../../components/Shared/ETWSCard'
+// import ETWSCard from '../../components/Shared/ETWSCard'
 // import Style from './Workshops.module.css'
 import Header from '../../components/Header/Header'
 import WorkshopsBanner from './WorkshopsBanner'
@@ -13,14 +13,6 @@ const Workshops = () => {
       {/* <Header /> */}
       <WorkshopsBanner />
       
-            <>
-              <div className="w-full flex items-center justify-around gap-4 flex-wrap"  >
-                <ETWSCard className="mx-4" />
-                <ETWSCard className="mx-4" />
-                <ETWSCard className="mx-4" />
-
-              </div>
-            </>
         
     </main>
   )
