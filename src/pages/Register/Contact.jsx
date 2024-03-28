c7b27d763040744f4d123dfd059db684bd071935
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-03-28 16:13:43 +0530

    idk

---
+++ src/pages/Register/Contact.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="absolute top-[14%] max-md:bottom-0">
+      <p className="text-gray-200 text-center">Facing any Issues? Contact Us</p>
+      <p className="text-gray-300 text-center">
+        Y.Siddartha +91 9398848215 codewithsiddhu@gmail.com{" "}
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -304,7 +304,7 @@ const RegisterForm = () => {
   }
 
   return (
-    <section className="z-30 w-full pt-[80px] pb-[20px]">
+    <section className="z-30 w-full pt-[160px] pb-[20px]">
       <form
         onSubmit={handleSubmit}
         className="w-[90%] max-w-[420px] mx-auto mb-20   rounded-md backdrop-filter backdrop-blur-lg px-3 py-2 flex justify-center items-center"
