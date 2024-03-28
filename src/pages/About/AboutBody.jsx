46f65e036f798e4a9e91b6be9a773cf532a59313
sribabu <sribabumandraju@gmail.com>
2024-03-28 18:52:29 +0530

    about

---
+++ src/pages/About/AboutBody.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import s1 from '../../assets/about/about.svg'
+
+
+const AboutBody = () => {
+  return (
+    <>
+        <div className="w-full min-h-[100vh]">
+            <div className="text-center  font-bold text-[40px] sm:text-[60px] md:text-[80px] text-white main-heading">ABOUT</div>
+            <div className="w-[97%] max-w-[1200px] border-[#f529a3] bg-[#200f22] border] flex justify-center items-center mx-auto min-h-[400px] backdrop-blur-lg relative ">
+                <div className="mx-3 z-[2]">
+                    <p className="px-3 mt-3" style={{textIndent:"30px"}}>Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed consequuntur doloremque corrupti aliquid vel. Perspiciatis praesentium ipsa quis numquam inventore beatae eveniet maiores hic nemo. Id amet reiciendis molestias! consectetur adipisicing elit. Nemo dolor ab modi sit fugiat! A perspiciatis ex deserunt illo, inventore alias nam nesciunt veritatis quaerat quae quasi rem consectetur quas?</p>
+                    <p className="px-3 mt-3" style={{textIndent:"30px"}}>Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed consequuntur doloremque corrupti aliquid vel. Perspiciatis praesentium ipsa quis numquam inventore beatae eveniet maiores hic nemo. Id amet reiciendis molestias! consectetur adipisicing elit. Nemo dolor ab modi sit fugiat! A perspiciatis ex deserunt illo, inventore alias nam nesciunt veritatis quaerat quae quasi rem consectetur quas?</p>
+                    <p className="px-3 mt-3" style={{textIndent:"30px"}}>Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed consequuntur doloremque corrupti aliquid vel. Perspiciatis praesentium ipsa quis numquam inventore beatae eveniet maiores hic nemo. Id amet reiciendis molestias! consectetur adipisicing elit. Nemo dolor ab modi sit fugiat! A perspiciatis ex deserunt illo, inventore alias nam nesciunt veritatis quaerat quae quasi rem consectetur quas?</p>
+                    <p className="px-3 mt-3 mb-3" style={{textIndent:"30px"}}>Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. A sed consequuntur doloremque corrupti aliquid vel. Perspiciatis praesentium ipsa quis numquam inventore beatae eveniet maiores hic nemo. Id amet reiciendis molestias! consectetur adipisicing elit. Nemo dolor ab modi sit fugiat! A perspiciatis ex deserunt illo, inventore alias nam nesciunt veritatis quaerat quae quasi rem consectetur quas?</p>
+                </div>               
+            </div>
+            <div className="text-center  font-bold text-[40px] sm:text-[60px] md:text-[80px] text-white main-heading">MEMORIES</div>
+            <div className="w-full my-3 mb-[80px]">
+            <div className="memos w-[97%]  max-w-[1200px] mx-auto ">
+                <a href="#" className="memo-1">
+                    <img src="assets/img/about/img01.webp" />
+                </a> 
+                <a href="#" className="memo-2">
+                    <img src="assets/img/about/img02.webp" />
+                </a>
+                <a href="#" className="memo-3">
+                    <img src="assets/img/about/img03.webp" />
+                </a>
+                <a href="#" className="memo-4">
+                    <img src="assets/img/about/img04.webp" />
+                </a>
+                <a href="#" className="memo-5">
+                    <img src="assets/img/about/img05.webp" />
+                </a>
+                <a href="#" className="memo-6">
+                    <img src="assets/img/about/img06.webp" />
+                </a>
+                <a href="#" className="memo-7">
+                    <img src="assets/img/about/img07.webp" />
+                </a>
+                <a href="#" className="memo-8">
+                    <img src="assets/img/about/img08.webp" />
+                </a>
+                <a href="#" className="memo-9">
+                    <img src="assets/img/about/img09.webp" />
+                </a>
+                <a href="#" className="memo-10">
+                    <img src="assets/img/about/img10.webp" />
+                </a>
+                <a href="#" className="memo-11">
+                    <img src="assets/img/about/img11.webp" />
+                </a>
+                <a href="#" className="memo-12">
+                    <img src="assets/img/about/img12.webp" />
+                </a>
+            </div>  
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default AboutBody
