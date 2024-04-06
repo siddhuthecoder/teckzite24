af60173350e07c531e65022aade83a82aca70cd8
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-04-06 18:59:42 +0530

    idk

--- src/pages/Register/Contact.jsx
+++ src/pages/Register/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
           Facing any Issues? Contact Us
         </p>
         <p className="text-gray-300 text-center">
-          H.Yasvanth +91 8330930503 yasvanthhanumantu1@gmail.com{" "}
+          G.Heamanth Kumar +91 8688917687 gujjalahemanthkumar789@gmail.com{" "}
         </p>
       </div>
       <div className="absolute bottom-[15%]">
