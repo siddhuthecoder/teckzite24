e8f23a958ef61cc2278650825680003a9a141fa0
sribabu <sribabumandraju@gmail.com>
2024-04-08 01:00:00 +0530

    shed

---
+++ src/pages/Schedule/scheduleConstants.js
@@ -0,0 +1,478 @@
+const Eve = {
+    "events" : [
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Icon Of Puc",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-3",
+            "Date": "14-04-2024",
+            "Time": "12.00 PM-12.45 PM"
+          }
+        ],
+        "Department": "PUC"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Debug Derby",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.45 AM-10.15 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "9.45 AM-10.15 AM"
+          }
+        ],
+        "Department": "PUC"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Logo Lexicon",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "10.30 AM-11.00 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.30 AM-11.00 AM"
+          }
+        ],
+        "Department": "PUC"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "The Silver Screen",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "11.15 AM-12.00 PM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "11.15AM-12.00 PM"
+          }
+        ],
+        "Department":  "PUC"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "ElectroFrenzy",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "13-04-2024",
+            "Time": "12.00 PM-1.00 PM"
+          }
+        ],
+        "Department": "ECE"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Business Pitching",
+        "Rounds": [
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "2.00 PM-5.00 PM"
+          }
+        ],
+        "Department": "ECE"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Electrospectra",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "10.30 AM-11.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.30 AM-11.30 AM"
+          }
+        ],
+        "Department": "ECE"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Hackathon",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-12.00 AM"
+          }
+        ],
+        "Department": "ECE"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Break The Query",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-10.00 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-10.00 AM"
+          }
+        ],
+        "Department": "CSE"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Code-O-Fiesta",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "10.15 AM-11.15 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.15 AM-11.15 AM"
+          }
+        ],
+        "Department": "CSE"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Web Design",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "12-04-2024",
+            "Time": "Last Submission Date"
+          }
+        ],
+        "Department": "CSE"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Techenigma",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "11.30 AM-12.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "11.30 AM-12.30 AM"
+          }
+        ],
+        "Department": "CSE"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Towers The Tolarate",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-10.00 AM"
+          }
+        ],
+        "Department": "CIVIL"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Poster Prowes",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-10.00 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.30 AM-11.15 AM"
+          }
+        ],
+        "Department": "CIVIL"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Optipore Analysis",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "10.30 AM-12.30 PM"
+          }
+        ],
+        "Department": "CIVIL"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Cad Maestro",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "11.30 AM-12.00 PM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "12.00 PM-1.00 PM"
+          }
+        ],
+        "Department": "CIVIL"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Mind Meld",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "13-04-2024",
+            "Time": "10.00 AM-11.00 AM"
+          }
+        ],
+        "Department": "CHEMICAL"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Powerpoint Presentation",
+        "Rounds": [
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.00 AM-11.00 AM"
+          }
+        ],
+        "Department":  "CHEMICAL"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Chem Quest",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "10.00 AM-11.00 AM"
+          }
+        ],
+        "Department":  "CHEMICAL"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Poster Presentation",
+        "Rounds": [
+          {
+            "Round": "Round-2",
+            "Date": "13-04-2024",
+            "Time": "11.00 AM-12.30 PM"
+          }
+        ],
+        "Department":  "CHEMICAL"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Cadomania",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-10.00AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "13-04-2024",
+            "Time": "10.00 AM-11.00 AM"
+          }
+        ],
+        "Department": "MECHANICAL"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Assembling & Dessembling",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-10.30AM"
+          }
+        ],
+        "Department": "MECHANICAL"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Think fast & calm first",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "10.30 AM-12.30 PM"
+          }
+        ],
+        "Department": "MECHANICAL"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Build boult builders",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "11.00AM-12.30 PM"
+          }
+        ],
+        "Department": "MECHANICAL"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "Metallography",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-9.30 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-12.30 PM"
+          }
+        ],
+        "Department": "MME"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Material Marathon",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.45AM- 11.15AM"
+          }
+        ],
+        "Department":  "MME"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Manufacturing mania",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "12.45PM-1.15PM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "12.15PM-12.45PM"
+          }
+        ],
+        "Department":  "MME"
+      },
+      {
+        "Event_no": 4,
+        "Name_of_the_Event": "Case Study",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "11.30AM-12.30PM"
+          }
+        ],
+        "Department":  "MME"
+      },
+      {
+        "Event_no": 1,
+        "Name_of_the_Event": "F1 2K24",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-11.00 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "13-04-2024",
+            "Time": "9.00 AM-11.00 AM"
+          }
+        ],
+        "Department": "EEE"
+      },
+      {
+        "Event_no": 2,
+        "Name_of_the_Event": "Escape quest",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "13-04-2024",
+            "Time": "11.00AM-12.00PM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "9.00 AM-10.00 AM"
+          }
+        ],
+        "Department": "EEE"
+      },
+      {
+        "Event_no": 3,
+        "Name_of_the_Event": "Circuit mania",
+        "Rounds": [
+          {
+            "Round": "Round-1",
+            "Date": "14-04-2024",
+            "Time": "10.00 AM-11.00 AM"
+          },
+          {
+            "Round": "Round-2",
+            "Date": "14-04-2024",
+            "Time": "11.00AM-12.00PM"
+          }
+        ],
+        "Department": "EEE"
+      }
+    ]
+  }
+  export default Eve
+  
