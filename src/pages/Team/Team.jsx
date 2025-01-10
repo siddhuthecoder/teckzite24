33866b386319b34b6133b3b378a5d845b1e32b0d
BinduSree-18 <palukurthibindu18@gmail.com>
2025-01-10 23:23:07 +0530

    hi

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Webteamicon from "../../assets/img/Webteamicon.png"
 const Team = () => {
   return (
-    <div className="flex justify-center items-center h-full w-full bg-black gap-10 flex-col md:flex-row pt-[10px] md:pt-0">
+    <div className="flex justify-center items-center h-full w-full bg-black gap-10 flex-col md:flex-row pt-[10px] md:pt-[50px]">
     <div className="flex bg-gray-500 w-[300px] h-[400px] justify-center items-center shadow-md transition-shadow duration-300 rounded-lg relative">
         <div className="flex bg-[#0A69A5] w-[280px] h-[380px] rounded-lg absolute hover:-translate-y-10 transition-transform duration-300 ease-in-out justify-center items-center">
             <div>
