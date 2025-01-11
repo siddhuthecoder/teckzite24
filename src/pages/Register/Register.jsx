5ad2026aa6486a2924639105ca2a869b3185bd03
Sireesha <n210413@rguktn.ac.in>
2025-01-11 01:40:26 -0800

    ki

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,7 +4,6 @@ import RegisterForm from "./RegisterForm";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Contact from "./Contact";
-
 const Register = () => {
   const userData = useSelector((state) => state.user.data);
   const navigate = useNavigate();
