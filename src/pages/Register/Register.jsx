5f1ec170f82930d094d121acbd15e2de940d20b3
Dharaneeswar Reddy Avula <avuladharaniswarreddy@gmail.com>
2025-01-12 14:15:15 +0530

    new

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -4,6 +4,7 @@ import RegisterForm from "./RegisterForm";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import Contact from "./Contact";
+import sbg from "../"
 const Register = () => {
   const userData = useSelector((state) => state.user.data);
   const navigate = useNavigate();
@@ -15,7 +16,15 @@ const Register = () => {
   }, [navigate, userData]);
 
   return (
-   <div className="w-full min-h-screen flex justify-center items-center">
+   <div className="w-full min-h-screen flex justify-center items-center relative"  style={{
+    backgroundImage:
+      "linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.7)), url('/assets/sbg.jpeg')",
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    minHeight: "100vh",
+  }}
+  >
       <Header />
       <RegisterForm />
       <Contact className="mt-[30px]" />
