ae678050b93933ebbfc8a88c9990f101a5e01c12
Dharaneeswar Reddy Avula <avuladharaniswarreddy@gmail.com>
2025-01-20 10:36:46 +0530

    new

--- src/pages/Register/RegisterForm.jsx
+++ src/pages/Register/RegisterForm.jsx
@@ -334,7 +334,7 @@ const RegisterForm = () => {
   return (
     <section className="z-5 w-full h-full pt-[160px] pb-[20px]"   style={{
       backgroundImage:
-        "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url('/assets/cbg.webp')",
+        "linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.7)), url('/eventdetailsbg.webp')",
       backgroundAttachment: "fixed",
       backgroundSize: "cover",
       backgroundPosition: "center",
