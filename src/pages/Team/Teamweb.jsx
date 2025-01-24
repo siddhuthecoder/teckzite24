fa5b443fc82774779a8ea5c00a6ce60a888f0e33
BinduSree-18 <palukurthibindu18@gmail.com>
2025-01-24 22:54:25 +0530

    teampage swiper

--- src/pages/Team/Teamweb.jsx
+++ src/pages/Team/Teamweb.jsx
@@ -74,7 +74,7 @@ const Teamweb = () => {
   useEffect(() => {
     const fetchTeamData = async () => {
       try {
-        const response = await fetch("https://tzm-1.onrender.com/api/teams"); 
+        const response = await fetch("https://tzm-1.onrender.com/api/teams");
         const data = await response.json();
         setTeam(data);
       } catch (error) {
@@ -94,6 +94,12 @@ const Teamweb = () => {
     centerMode: true, // Keeps the middle card in focus
     centerPadding: "0px",
     focusOnSelect: true, // Allows clicking on slides to focus
+    centerMode: true,  // Keeps the middle slide in focus
+    centerPadding: "0px",
+
+    autoplay: true, // Enables automatic sliding
+    autoplaySpeed: 1400, // Slides every 2 seconds
+    pauseOnHover: true, // Stops autoplay when hovered
     responsive: [
       {
         breakpoint: 768,
@@ -117,24 +123,24 @@ const Teamweb = () => {
     >
       <Header />
       <div className="flex  justify-center items-center gap-4 py-[100px]">
-      <Slider {...settings} className="w-[80%] h-full mt-[70px]">
-        {team.map((member) => (
-          <div key={member._id} className="relative">
-            <img src={Teamcard} className="w-80 h-96" alt="Team Card" />
-            <img
-              src={member.image}
-              className="absolute w-52 h-52 top-16 left-16 object-cover rounded-full"
-              alt={member.name}
-            />
-            <p className="absolute ml-[70px] bottom-6 text-white">
-              {member.name}
-            </p>
-            <p className="absolute bottom-1 ml-[70px] font-bold text-[black]">
-              {member.position}
-            </p>
-          </div>
-          
-        ))}
+        <Slider {...settings} className="w-[80%] h-full md:mt-[170px] mt-[80px] lg:mt-[70px]">
+          {team.map((member) => (
+            <div key={member._id} className="relative">
+              <img src={Teamcard} className="w-80 h-96" alt="Team Card" />
+              <img
+                src={member.image}
+                className="absolute lg:w-52 lg:h-52 w-52 h-52 md:w-[150px] md:h-[150px] md:top-24 md:left-8 lg:top-16 top-16 left-10 lg:left-14  rounded-full"
+                alt={member.name}
+              />
+              <p className="absolute ml-[70px] bottom-6 text-white">
+                {member.name}
+              </p>
+              <p className="absolute bottom-1 ml-[70px] font-bold text-[black]">
+                {member.position}
+              </p>
+            </div>
+
+          ))}
         </Slider>
       </div>
     </div>
