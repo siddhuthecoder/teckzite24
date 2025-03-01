1a6095fefec3243bfef710dcfd0957944e89583a
DharaneeswarDharaneeswar-Reddy-Avula <avuladharaniswarreddy@gmail.com>
2025-03-01 15:19:39 +0530

    dhanu

--- src/pages/Hackathon/Hacktype.jsx
+++ src/pages/Hackathon/Hacktype.jsx
@@ -5,7 +5,20 @@ const Hacktype = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex gap-[30px] mb-[100px] flex-wrap">
+    <div className="px-[100px]">
+      <div className="flex flex-col  py-[50px] px-[30px] rounded-lg mb-[30px]  backdrop-blur-lg bg-white/10 border border-white/20 shadow-lg text-white ">
+        <h2 className="text-3xl">About Hacakathon</h2>
+      Teckzite'25 Hackathons, part of South India's largest techno-management
+fest at RGUKT Nuzvid, bring together innovative minds to solve real-world
+challenges through two exciting tracks: AI Hackathon and Robotic
+Hackathon. Participants will experience expert mentorship, industry
+exposure, and national-level competition. Top performers will earn
+recognition, exciting prizes, and networking opportunities. Get ready for an
+exhilarating coding experience, collaborate with tech enthusiasts, and push
+your creative limits as you develop cutting-edge AI and robotic solutions at
+Teckzite'25 Hackathons.
+      </div>
+    <div className="flex gap-[30px] mb-[100px] flex-wrap justify-center">
       <div
         className="h-[400px] w-[300px] flex justify-center items-center text-2xl font-extrabold
                    backdrop-blur-lg bg-white/10 border border-white/20 shadow-lg 
@@ -35,6 +48,11 @@ const Hacktype = () => {
       >
         <h2>Robotics Hackathon</h2>
       </div>
+    </div>
+    <div className="flex flex-wrap gap-[30px] mb-[50px] justify-center">
+      <img src="timeline.png" className="rounded-lg"/>
+      <img src="prizepool.png" className="rounded-lg"/>
+    </div>
     </div>
   );
 };
